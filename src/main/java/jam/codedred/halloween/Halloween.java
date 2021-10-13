67b16df47ce4a0da92b4e34b24b994dd1723b36c
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 20:09:45 +0200

    Initial commit

---
+++ src/main/java/jam/codedred/halloween/Halloween.java
@@ -0,0 +1,17 @@
+package jam.codedred.halloween;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Halloween extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
