0633aa0ca4c3a6966e82bd19027b3338ce06aee2
mabidux <70872559+mabidux@users.noreply.github.com>
2021-10-14 14:52:30 -0400

    Add files via upload

--- src/main/java/jam/codedred/halloween/Halloween.java
+++ src/main/java/jam/codedred/halloween/Halloween.java
@@ -1,9 +1,16 @@
 package jam.codedred.halloween;
 
-import jam.codedred.halloween.commands.SpawnCommand;
-import jam.codedred.halloween.commands.StartCommand;
+import jam.codedred.halloween.utils.CommandInformation;
+
+import java.io.IOException;
+
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
 
 public final class Halloween extends JavaPlugin {
 
@@ -13,13 +20,50 @@ public final class Halloween extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         INSTANCE = this;
-
-        getCommand("spawn").setExecutor(new SpawnCommand());
-        getCommand("start").setExecutor(new StartCommand());
+        
+        loadCommands();
+        loadEvents();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+    
+    private void loadCommands() {
+    	try {
+			for (final ClassInfo classInfo : ClassPath.from(getClassLoader()).getTopLevelClassesRecursive("jam.codedred.halloween.commands")) {
+				final Class<?> cls = Class.forName(classInfo.getName());
+				if (CommandExecutor.class.isAssignableFrom(cls) && cls.isAnnotationPresent(CommandInformation.class)) {
+					try {
+						getCommand(cls.getDeclaredAnnotation(CommandInformation.class).value()).setExecutor((CommandExecutor) cls.newInstance());
+					} catch (InstantiationException | IllegalAccessException e) {
+						Bukkit.getConsoleSender().sendMessage("§cCommand in class " + cls.getName() + " couldn't be loaded.");
+						continue;
+					}
+				}
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			Bukkit.getConsoleSender().sendMessage("§cHalloween plugin wasn't able load commands.");
+		}
+    }
+    
+    private void loadEvents() {
+    	try {
+			for (final ClassInfo classInfo : ClassPath.from(getClassLoader()).getTopLevelClassesRecursive("jam.codedred.halloween.events")) {
+				final Class<?> cls = Class.forName(classInfo.getName());
+				if (Listener.class.isAssignableFrom(cls)) {
+					try {
+						Bukkit.getPluginManager().registerEvents((Listener) cls.newInstance(), this);
+					} catch (InstantiationException | IllegalAccessException e) {
+						Bukkit.getConsoleSender().sendMessage("§cEvents in class " + cls.getName() + " couldn't be loaded.");
+						continue;
+					}
+				}
+			}
+		} catch (ClassNotFoundException | IOException e) {
+			Bukkit.getConsoleSender().sendMessage("§cHalloween plugin wasn't able load events.");
+		}
+    }
+    
 }
