3d1f6f3a892077ff7c916bb34bea6cf1f70826e4
mabidux <70872559+mabidux@users.noreply.github.com>
2021-10-14 14:53:10 -0400

    Add files via upload

---
+++ src/main/java/jam/codedred/halloween/utils/CommandInformation.java
@@ -0,0 +1,14 @@
+package jam.codedred.halloween.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CommandInformation {
+	
+	public String value();
+	
+}
